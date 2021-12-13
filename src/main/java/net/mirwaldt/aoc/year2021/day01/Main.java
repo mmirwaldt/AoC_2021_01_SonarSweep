3cece50d42abc4c2e9e0d4ab0c15387b9c6f78f2
mmirwaldt <michael.mirwaldt@gmail.com>
2021-12-13 22:44:02 +0100

    solved puzzle part one

---
+++ src/main/java/net/mirwaldt/aoc/year2021/day01/Main.java
@@ -0,0 +1,18 @@
+package net.mirwaldt.aoc.year2021.day01;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // part one
+        final List<String> lines = Files.readAllLines(Path.of("input"), StandardCharsets.US_ASCII);
+        List<Integer> depths = lines.stream().map(Integer::parseInt).collect(Collectors.toList());
+        SonarSweeper sonarSweeper = new LoopSonarSweeper();
+        System.out.println(sonarSweeper.countIncreases(depths)); // result: 1665
+    }
+}
